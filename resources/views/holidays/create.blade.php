6cbce42acf5117606f01192e6049f9d5529f2fb2
Josephus21 <jabatatayo21@gmail.com>
2026-03-14 12:45:23 +0800

    css

--- resources/views/holidays/create.blade.php
+++ resources/views/holidays/create.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.dashboard')
+
+
 <div class="mb-3">
     <label class="form-label">Holiday Name</label>
     <input type="text" name="name" class="form-control" value="{{ old('name', $holiday->name ?? '') }}" required>
